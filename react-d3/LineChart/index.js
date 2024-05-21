8562f750613cd9ade16bdd66eea84eb10ef2d4d6
Kevin Schaul <kevin.schaul@gmail.com>
2024-05-21 13:22:26 -0500

    better looking chart

--- react-d3/LineChart/index.js
+++ react-d3/LineChart/index.js
@@ -1,29 +1,24 @@
 import { useEffect, useRef } from "react";
 import { useResizeDetector } from "react-resize-detector";
 import { select } from "d3-selection";
-import { extent } from "d3-array";
-import { scaleLinear } from "d3-scale";
+import { extent, max } from "d3-array";
+import { scaleLinear, scaleTime } from "d3-scale";
 import { line } from "d3-shape";
-
-const data = [
-  { year: 2020, value: 10 },
-  { year: 2021, value: 8 },
-  { year: 2022, value: 6 },
-  { year: 2023, value: 14 },
-]
+import { axisBottom, axisLeft } from "d3-axis";
 
 const LineChart = (props) => {
+  const { data } = props;
   const { width, height, ref } = useResizeDetector();
 
   const margin = {
     top: 10,
-    right: 10,
-    bottom: 10,
-    left: 10,
+    right: 30,
+    bottom: 20,
+    left: 30,
   };
 
   const chartWidth = width - margin.left - margin.right;
-  const chartHeight = (chartWidth * 0.5) - margin.top - margin.bottom;
+  const chartHeight = chartWidth * 0.4 - margin.top - margin.bottom;
 
   useEffect(() => {
     const svg = select(ref.current).select("svg").html("");
@@ -32,26 +27,40 @@ const LineChart = (props) => {
       .append("g")
       .attr("transform", `translate(${margin.left},${margin.top})`);
 
-    const x = scaleLinear()
-      .domain(extent(data, d => d.year))
-      .range([0, chartWidth])
+    const x = scaleTime()
+      .domain(extent(data, (d) => d.date))
+      .range([0, chartWidth]);
 
     const y = scaleLinear()
-      .domain(extent(data, d => d.value))
-      .range([chartHeight, 0])
+      .domain([0, max(data, (d) => d.rate)])
+      .range([chartHeight, 0]);
+
+    const gY = g.append("g").call(axisLeft(y).tickSize(-width).ticks(5));
+
+    gY.selectAll(".tick line").style("stroke", "#ccc");
+
+    gY.selectAll(".domain").remove();
+
+    const gX = g
+      .append("g")
+      .attr("transform", `translate(0, ${chartHeight})`)
+      .call(axisBottom(x));
+
+    gX.selectAll(".tick line").remove();
+    gX.selectAll(".domain").remove();
 
     const l = line()
-      .x(d => x(d.year))
-      .y(d => y(d.value))
+      .x((d) => x(d.date))
+      .y((d) => y(d.rate));
 
-    g.selectAll('path')
+    g.append("g")
+      .selectAll("path")
       .data([data])
-      .join('path')
-      .attr('d', l)
-      .style('fill', 'none')
-      .style('stroke', '#000')
-      .style('stroke-width', 3)
-    
+      .join("path")
+      .attr("d", l)
+      .style("fill", "none")
+      .style("stroke", "#000")
+      .style("stroke-width", 3);
   }, [margin]);
 
   return (
